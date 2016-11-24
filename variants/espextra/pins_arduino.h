0d13cf04e421c43ca18e64f821b7e5c0e0d3baf7
Norbert Fekete <norbert.fekete@ericsson.com>
2016-11-24 15:10:42 +0100

    Changed pin consts to #defines so they can be used in macros

--- variants/espextra/pins_arduino.h
+++ variants/espextra/pins_arduino.h
@@ -53,67 +53,67 @@ extern "C" {
 
 // ESP8266 pins
 
-static const uint8_t CS_MCP23S17 = 15;  // D8: TX1/HCS (pull down on boot)
-static const uint8_t CS_MCP3008  =  2;  // D4: bootstrap TX/blue LED (pull up on boot)
-static const uint8_t CS_0        =  4;  // D2
-static const uint8_t CS_1        =  0;  // D3: flash (pull up on boot)
-static const uint8_t CS_2        = 16;  // D0: user/wake/red LED
+#define CS_MCP23S17  15  // D8: TX1/HCS (pull down on boot)
+#define CS_MCP3008    2  // D4: bootstrap TX/blue LED (pull up on boot)
+#define CS_0          4  // D2
+#define CS_1          0  // D3: flash (pull up on boot)
+#define CS_2         16  // D0: user/wake/red LED
 
-static const uint8_t INT_GPIO    =  5;  // D1
+#define INT_GPIO      5  // D1
 
-static const uint8_t SDA         =  4;  // CS_0 / D2
-static const uint8_t SCL         =  0;  // CS_1 / D3
+#define SDA           4  // CS_0 / D2
+#define SCL           0  // CS_1 / D3
 
 // Expanded pins
 
-static const uint8_t LED_BUILTIN = NUM_INTERNAL_PINS;
-static const uint8_t BUILTIN_LED = NUM_INTERNAL_PINS;
+#define LED_BUILTIN  (NUM_INTERNAL_PINS)
+#define BUILTIN_LED  (NUM_INTERNAL_PINS)
 
 // PORTA
-static const uint8_t D0  = NUM_INTERNAL_PINS;
-static const uint8_t D1  = NUM_INTERNAL_PINS + 1;
-static const uint8_t D2  = NUM_INTERNAL_PINS + 2;
-static const uint8_t D3  = NUM_INTERNAL_PINS + 3;
-static const uint8_t D4  = NUM_INTERNAL_PINS + 4;
-static const uint8_t D5  = NUM_INTERNAL_PINS + 5;
-static const uint8_t D6  = NUM_INTERNAL_PINS + 6;
-static const uint8_t D7  = NUM_INTERNAL_PINS + 7;
+#define D0  (NUM_INTERNAL_PINS)
+#define D1  (NUM_INTERNAL_PINS + 1)
+#define D2  (NUM_INTERNAL_PINS + 2)
+#define D3  (NUM_INTERNAL_PINS + 3)
+#define D4  (NUM_INTERNAL_PINS + 4)
+#define D5  (NUM_INTERNAL_PINS + 5)
+#define D6  (NUM_INTERNAL_PINS + 6)
+#define D7  (NUM_INTERNAL_PINS + 7)
 // PORTB
-static const uint8_t D8  = NUM_INTERNAL_PINS + 8;
-static const uint8_t D9  = NUM_INTERNAL_PINS + 9;
-static const uint8_t D10 = NUM_INTERNAL_PINS + 10;
-static const uint8_t D11 = NUM_INTERNAL_PINS + 11;
-static const uint8_t D12 = NUM_INTERNAL_PINS + 12;
-static const uint8_t D13 = NUM_INTERNAL_PINS + 13;
-static const uint8_t D14 = NUM_INTERNAL_PINS + 14;
-static const uint8_t D15 = NUM_INTERNAL_PINS + 15;
+#define D8   (NUM_INTERNAL_PINS + 8)
+#define D9   (NUM_INTERNAL_PINS + 9)
+#define D10  (NUM_INTERNAL_PINS + 10)
+#define D11  (NUM_INTERNAL_PINS + 11)
+#define D12  (NUM_INTERNAL_PINS + 12)
+#define D13  (NUM_INTERNAL_PINS + 13)
+#define D14  (NUM_INTERNAL_PINS + 14)
+#define D15  (NUM_INTERNAL_PINS + 15)
 // PORTC
-static const uint8_t D16 = NUM_INTERNAL_PINS + 16;
-static const uint8_t D17 = NUM_INTERNAL_PINS + 17;
-static const uint8_t D18 = NUM_INTERNAL_PINS + 18;
-static const uint8_t D19 = NUM_INTERNAL_PINS + 19;
-static const uint8_t D20 = NUM_INTERNAL_PINS + 20;
-static const uint8_t D21 = NUM_INTERNAL_PINS + 21;
-static const uint8_t D22 = NUM_INTERNAL_PINS + 22;
-static const uint8_t D23 = NUM_INTERNAL_PINS + 23;
+#define D16  (NUM_INTERNAL_PINS + 16)
+#define D17  (NUM_INTERNAL_PINS + 17)
+#define D18  (NUM_INTERNAL_PINS + 18)
+#define D19  (NUM_INTERNAL_PINS + 19)
+#define D20  (NUM_INTERNAL_PINS + 20)
+#define D21  (NUM_INTERNAL_PINS + 21)
+#define D22  (NUM_INTERNAL_PINS + 22)
+#define D23  (NUM_INTERNAL_PINS + 23)
 // PORTD
-static const uint8_t D24 = NUM_INTERNAL_PINS + 24;
-static const uint8_t D25 = NUM_INTERNAL_PINS + 25;
-static const uint8_t D26 = NUM_INTERNAL_PINS + 26;
-static const uint8_t D27 = NUM_INTERNAL_PINS + 27;
-static const uint8_t D28 = NUM_INTERNAL_PINS + 28;
-static const uint8_t D29 = NUM_INTERNAL_PINS + 29;
-static const uint8_t D30 = NUM_INTERNAL_PINS + 30;
-static const uint8_t D31 = NUM_INTERNAL_PINS + 31;
+#define D24  (NUM_INTERNAL_PINS + 24)
+#define D25  (NUM_INTERNAL_PINS + 25)
+#define D26  (NUM_INTERNAL_PINS + 26)
+#define D27  (NUM_INTERNAL_PINS + 27)
+#define D28  (NUM_INTERNAL_PINS + 28)
+#define D29  (NUM_INTERNAL_PINS + 29)
+#define D30  (NUM_INTERNAL_PINS + 30)
+#define D31  (NUM_INTERNAL_PINS + 31)
 
-static const uint8_t A1 = NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS;
-static const uint8_t A2 = NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 1;
-static const uint8_t A3 = NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 2;
-static const uint8_t A4 = NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 3;
-static const uint8_t A5 = NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 4;
-static const uint8_t A6 = NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 5;
-static const uint8_t A7 = NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 6;
-static const uint8_t A8 = NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 7;
+#define A1  (NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS)
+#define A2  (NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 1)
+#define A3  (NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 2)
+#define A4  (NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 3)
+#define A5  (NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 4)
+#define A6  (NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 5)
+#define A7  (NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 6)
+#define A8  (NUM_DIGITAL_PINS + NUM_INTERNAL_ANALOG_INPUTS + 7)
 
 #ifdef __cplusplus
 } // extern "C"
